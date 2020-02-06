9165ff47773316747a3933bf82953f72269792ce
PC-LARS\LarsV <larsvolkers@hotmail.nl>
2020-02-06 19:57:45 +0100

    Improved editor experience

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,23 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['airbnb', 'prettier', 'prettier/@typescript-eslint'],
-  plugins: ['prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: './'
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: ['prettier', '@typescript-eslint', 'import'],
   rules: {
     'prettier/prettier': ['error']
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
   }
 };
