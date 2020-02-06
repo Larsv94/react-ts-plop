1eaf94a13fde2ee6b20e95227d8dbbeadcd9afe3
PC-LARS\LarsV <larsvolkers@hotmail.nl>
2020-02-06 20:16:29 +0100

    Removed path aliases

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-require('ts-node').register({
-  compilerOptions: {
-    module: 'commonjs',
-    esModuleInterop: true,
-    allowSyntheticDefaultImports: true,
-    target: 'es6',
-    noImplicitAny: true,
-    moduleResolution: 'node',
-    sourceMap: true,
-    outDir: 'dist',
-    baseUrl: '.',
-    paths: {
-      '*': ['node_modules/*', 'src/types/*']
-    }
-  },
-  include: ['src/**/*']
-});
+require('ts-node').register();
 
 module.exports = require('./src/index');
